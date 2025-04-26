ec3becc48807837e6dc24793ca22f681e5947cd3
Rafael <rafael.felix.moraes@gmail.com>
2025-04-26 18:53:45 -0300

    "Feat Adicionado Laços"

---
+++ resources/views/locos.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laçõs</title>
+</head>
+<body>
+    <h1>Laços de repetição com Blade</h1>
+
+    <h2>FOR</h2>
+    <p>Adicionamos @ antes do for e finalizamos com @ endfore não abrimos o blocos de chaves.</p>
+    <p>@ for($i=1; $i<10; $i++)></p>
+    <p>pConteúdo a ser exibir ou modificado./p</p>
+    <p>@ endfor</p>
+
+    <h2>FOREACH</h2>
+    <p></p>
+    
+</body>
+</html>
