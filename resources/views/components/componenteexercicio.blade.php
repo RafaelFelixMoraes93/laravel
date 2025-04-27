7df72c839d29d11985027a04f2fa23d841a686f9
Rafael <rafael.felix.moraes@gmail.com>
2025-04-27 00:29:51 -0300

    Feat Component exercicio

---
+++ resources/views/components/componenteexercicio.blade.php
@@ -0,0 +1,20 @@
+<div style="
+    width: 200px;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin: 20px;
+    background-color: #eeee;
+">
+
+<img src="{{$image}}">
+
+<div style="padding: 20px;">
+    <h1>{{$nome}}</h1>
+    <h3>{{$idade}} Anos</h3>
+    <p>Nasceu em {{$birth}}</p>
+</div>
+
+</div>
+
