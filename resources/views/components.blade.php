ff2b6f99427bee8dd7355ca55e638ba77540f32f
Rafael <rafael.felix.moraes@gmail.com>
2025-04-27 00:29:12 -0300

    Feat arquivo components

--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -16,5 +16,35 @@
     @foreach ($ingredientes as $ing)
         <p>{{$ing}} <button style="background-color: rgb(192, 28, 28); color: white; border: 0cap; padding: 5px;">Deletar</button></p>
     @endforeach
+
+    <h2>Transformando o component em reutilizavel e flexivél</h2>
+    <p>Dentro de uma component usamos $slot dentro de chaves duplas.</p>
+    <p>Quando formos modificar colocamos direto no html.</p>
+    <p>@ component"("nome_da_pasta.nome_do_component")"</p>
+    Editar
+    <p>@ endcomponent</p>
+    <p>@ component"("nome_da_pasta.nome_do_component")"</p>
+    Deletar
+    <p>@ endcomponent</p>    
+
+    <h2>Deixando as cores com variáveis</h2>
+    <p>Para passar a cor, utiliza-se @ slot e fecha-se com @ endslot</p>
+    @foreach ($ingredientes as $ing)
+        <p>{{ $ing }} -
+            @component('components.botao')
+                @slot('cor')
+                    blue
+                @endslot
+                Editar
+            @endcomponent
+
+            @component('components.botao')
+                @slot('cor')
+                    red
+                @endslot
+                Deletar
+            @endcomponent
+        </p>
+    @endforeach
 </body>
 </html>
